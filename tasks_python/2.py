6eeb3436b0d9041cbfb04c22605a0904d52ba239
Ilya Andrianov <ilandroxxy@gmail.com>
2023-07-18 04:43:05 +0700

    Я создал три пустых папки для работы с экзаменом ЕГЭ:
    - Одна папка для работы с задачами на прогу
    
    - Еще одна для работы с excel файлами (добавляйте сюда свои таблицы)
    
    - И третья для 5-ти пиьсменных номеров (сюда можно сохранять картинки с решениями)

---
+++ tasks_python/2.py
@@ -0,0 +1,11 @@
+# Раскомментируйте код и вместо pass подставьте свои значения
+"""
+print('x y z w F')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = pass
+                if F == pass:
+                    print(x, y, z, w, F)
+"""
